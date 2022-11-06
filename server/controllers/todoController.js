1a04c4515fe1eadda4336bf09e57f9cb9f5485b6
headbwoi <tochukwujohn24@gmail.com>
2022-11-06 16:59:27 +0100

    moved logic to todoController

---
+++ server/controllers/todoController.js
@@ -0,0 +1,21 @@
+const getAllTodos = (req, res) => {
+  res.status(200).json({ message: "Hello to todo app" })
+}
+const getOneTodo = (req, res) => {
+  const { id } = req.params
+  res.status(200).json({ message: `todo of ${id} is ${id}` })
+}
+const createTodo = (req, res) => {
+  const { id, title, todo } = req.body
+  res.status(201).json({ id, title, todo })
+}
+const updateTodo = (req, res) => {
+  const { id } = req.params
+  const { title, todo } = req.body
+  res.json({ message: `todo with ${id} ${title} ${todo}, updated ` })
+}
+
+const deleteTodo = (req, res) => {
+  const { id } = req.params
+  res.status(200).json({ message: `todo with ${id} deleted successfully` })
+}
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -3,25 +3,10 @@ import express from "express"
 const router = express.Router()
 
 router
-  .get("/todos", (req, res) => {
-    res.status(200).json({ message: "Hello to todo app" })
-  })
-  .get("/todos/:id", (req, res) => {
-    const { id } = req.params
-    res.status(200).json({ message: `todo of ${id} is ${id}` })
-  })
-  .post("/todos", (req, res) => {
-    const { id, title, todo } = req.body
-    res.status(201).json({ id, title, todo })
-  })
-  .put("/todos/:id", (req, res) => {
-    const { id } = req.params
-    const { title, todo } = req.body
-    res.json({ message: `todo with ${id} ${title} ${todo}, updated ` })
-  })
-  .delete("/todos/:id", (req, res) => {
-    const { id } = req.params
-    res.status(200).json({ message: `todo with ${id} deleted successfully` })
-  })
+  .get("/todos", )
+  .get("/todos/:id", )
+  .post("/todos", )
+  .put("/todos/:id", )
+  .delete("/todos/:id", )
 
 export default router
