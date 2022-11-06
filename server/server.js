75bdf90cc0a81a790f34b929acc791e12d6127cb
headbwoi <tochukwujohn24@gmail.com>
2022-11-06 16:28:48 +0100

    started on backend created mvc architecture

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 
-app.get('/', (req, res) => res.send('Hello World!'))
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => res.send("Hello World!"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
