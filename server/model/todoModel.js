1aac4a4586381ff29f1268fc743049826bb012df
headbwoi <tochukwujohn24@gmail.com>
2022-11-07 15:53:37 +0100

    setup tod schema

--- server/model/todoModel.js
+++ server/model/todoModel.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose"
+
+const todoSchema = new mongoose.Schema({
+  id: String,
+
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model("TodoSchema", todoSchema)
